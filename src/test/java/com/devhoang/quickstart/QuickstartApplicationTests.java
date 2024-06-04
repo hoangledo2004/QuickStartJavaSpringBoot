0eced0e26a02e2546f8c08172bcbc4ea84633303
Hoang Le <hoangledo2092004@gmail.com>
2024-06-04 15:05:41 +0700

    Initial commit for Quickstart Java Spring Boot.

---
+++ src/test/java/com/devhoang/quickstart/QuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devhoang.quickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
