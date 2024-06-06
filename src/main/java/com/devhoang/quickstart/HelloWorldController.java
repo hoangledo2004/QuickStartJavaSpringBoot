0100fb61b63c905e7322470f9b56548fee49a229
Hoang Le <hoangledo2092004@gmail.com>
2024-06-06 16:44:34 +0700

    Hoang Le: Introducing Beans.

--- src/main/java/com/devhoang/quickstart/HelloWorldController.java
+++ src/main/java/com/devhoang/quickstart/HelloWorldController.java
@@ -30,4 +30,50 @@ public class HelloWorldController {
     // test -- runs unit tests
     // package -- creates a jar or war file
     // verify -- run checks & integration tests
+
+
+    // *******************************
+    // Maven Spring Boot Plugin
+    // another way to run our application file
+    // ./mvnw spring-boot:run
+
+    // *******************************
+    // Spring Framework and Spring Boot
+    // Spring Boot is a popular framework for building java applications.
+    // A framework is a chunk of code written on top of a language's core
+    // library to solve common problems.
+
+    // Spring Framework
+    // connect to db, expressing RESTApi
+    // it is highly configurable -- takes efforts
+
+    // Spring Boot is built on top of Spring Framework
+    // so using Spring Boot do all the configurations for you
+
+    // *******************************
+    // Spring App Layers
+    // Layers: Presentation - Service - Persistence
+
+    // Persistence Layer
+    // Entities -- Java Object, NoSQL
+    // Repositories/DAOs (Data Access Objects)
+    // CRUD
+
+    // Service Layer
+    // Presentation Layer
+
+
+    // *******************************
+    // Modularity
+
+    // *******************************
+    // Introducing Beans
+    //
+    // *******************************
+    // Annotation
+    // @SpringBootApplication
+    // @Configuration -- this identifies a configuration class, which is somewhere Spring should look for bean during its component scanning phrase
+    // @ComponentScan -- look for beans and placed that beans are needed
+    // @EnableAutoConfiguration -- providing sensible default
+
 }
