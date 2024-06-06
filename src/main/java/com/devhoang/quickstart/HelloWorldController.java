6efa2ee2f56598d24e86acb07d40a9636b0c4925
Hoang Le <hoangledo2092004@gmail.com>
2024-06-06 14:56:08 +0700

    Hoang Le: Maven Workflow.

--- src/main/java/com/devhoang/quickstart/HelloWorldController.java
+++ src/main/java/com/devhoang/quickstart/HelloWorldController.java
@@ -11,4 +11,23 @@ public class HelloWorldController {
     public String helloWorld() {
         return "Hello World!";
     }
+
+    // mvnw [options] [<goals(s)>] [<phrase(s)>]
+    // Maven Concepts:
+    // mvnw
+    // clean -- remove temporary directories and files
+    // default -- where the most useful goals live
+    // site -- where documentation is generated
+
+
+    // mvnw clean
+    // pre-clean -- hook for before cleaning
+    // clean -- does the actual cleaning
+    // post-clean -- hook for after cleaning
+
+    // mvnw default
+    // compile -- compile your code into bytecode
+    // test -- runs unit tests
+    // package -- creates a jar or war file
+    // verify -- run checks & integration tests
 }
